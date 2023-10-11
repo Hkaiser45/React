c6dc93bb45acb02cbbbdbefdd6e9b2f4e7548d2c
Elie Rakoto <deicide21@hotmail.fr>
2023-10-11 11:54:33 +0200

    props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,9 @@
-const pokemonList = [
+function PokemonCard ({pokemon}) {
+  
 
-    {
   
-      name: "bulbasaur",
-  
-      imgSrc:
-  
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  
-    },
-  
-    {
-  
-      name: "mew",
-  
-    },
-  
-  ];
 
-
-
-
-
-function PokemonCard () {
-    let pokemon = pokemonList[0];
-    return <figure>{ pokemon.imgSrc?
+    return <figure>{pokemon.imgSrc?
         <img src={pokemon.imgSrc}></img> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
         </figure> 
